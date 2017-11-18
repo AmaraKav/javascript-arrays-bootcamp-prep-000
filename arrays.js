31dba3ffcf7ede3a89263ccc221e64b7a84f55d5
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-18 22:23:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function accessElementInArray(array, index){
  return array[index]; 
 }
 
-function destructivelyRemoveElementToBeginningOfArray(array, element){
+function destructivelyRemoveElementFromBeginningOfArray(array, element){
   array.shift(element);
   return array;
 }
