237cac4ed01638cad91faf91f494b81057341644
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-18 22:12:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element);
   return array;
 }
+
+function accessElementInArray(array, index){
+ return array[index]; 
+}
