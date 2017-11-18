9525fb49f76cc8892e76f324aeff35d719e5a14a
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-18 23:02:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyAddElementToEndOfArray(array, element){
 }
 
 function removeElementFromEndOfArray(array){
-  array.slice(0, array.length - 1);
+  array.slice(-1);
   return array;
 }
